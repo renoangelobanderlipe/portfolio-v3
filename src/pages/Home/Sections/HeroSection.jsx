0274515568cede232991a8af35afe1baad175fb5
Reno Angelo <personal.renoangelo@gmail.com>
2023-12-05 00:48:12 +0800

    refs #3 moved mobile first appraoch

--- src/pages/Home/Sections/HeroSection.jsx
+++ src/pages/Home/Sections/HeroSection.jsx
@@ -1,6 +1,4 @@
-import { Icon } from '@iconify/react';
 import { Button, Typography } from '@material-tailwind/react';
-import React from 'react';
 import { EnvelopeIcon, RightArrowIcon } from '../../../utils/icons';
 import Lottie from 'lottie-react';
 import HeroSectionLottie from '../../../assets/json/hero-section.json'
@@ -9,34 +7,31 @@ import { AppLayout } from '../../../components/ui/AppLayout';
 export const HeroSection = () => {
   return (
     <AppLayout>
-      {/* left */}
-      <div className="flex flex-col  gap-[48px] w-full">
-        {/* heading */}
+      <div className="flex flex-col gap-[48px] w-full">
         <div className="flex flex-col">
           <Typography
             variant="h5"
-            className="flex items-center font-fira text-primary capitalize"
+            className="flex items-center font-fira font-[16px] lg:font-[24px] text-primary capitalize"
           >
             Hi, I'm Reno Angelo
           </Typography>
           <Typography
             variant="h1"
-            className=" max-w-[535px] flex items-center font-main text-text capitalize font-[700] text-[64px]">
+            className=" text-[48px] lg:text-[64px] font-[700] max-w-[535px] flex items-center font-main text-text capitalize">
             Lorem Ipsum dolor sit amet
           </Typography>
         </div>
 
-        {/* buttons */}
-        <div className='flex gap-[24px]'>
-          <Button variant="text" className="flex gap-2 rounded-full shadow-none px-0">
+        <div className='flex flex-col-reverse lg:flex-row gap-[24px]'>
+          <Button variant="text" className="flex gap-2 justify-center lg:justify-start rounded-full shadow-none px-0 ">
             <RightArrowIcon />
-            <Typography className="flex items-center font-fira text-primary capitalize">
+            <Typography className="flex items-center font-fira text-[18px] text-primary capitalize">
               Browse My Works
             </Typography>
           </Button>
-          <Button variant="filled" className="flex gap-2 rounded-full bg-primary shadow-none">
+          <Button variant="filled" className="flex gap-2 rounded-full justify-center lg:justify-start bg-primary shadow-none">
             <EnvelopeIcon />
-            <Typography className="flex items-center font-fira text-icon capitalize">
+            <Typography className="flex items-center font-fira text-[18px] text-icon capitalize">
               Reach out to me
             </Typography>
           </Button>
@@ -44,7 +39,6 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      {/* right, lottie */}
       <div className="w-full flex items-center">
         <Lottie animationData={HeroSectionLottie} loop={true} width={'150px'} />
       </div>

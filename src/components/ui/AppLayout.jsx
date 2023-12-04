e1ddcb800987a6f193161c3e24d147d009d2bae0
Reno Angelo <personal.renoangelo@gmail.com>
2023-12-05 00:51:48 +0800

    refs #13 App Layout

--- src/components/ui/AppLayout.jsx
+++ src/components/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const AppLayout = ({ children, hasImage = true }) => {
   let bgImage = hasImage ? "bg-[url('bg-pattern.png')]" : "bg-bgColor";
   return (
-    <div className={`${bgImage} py-[100px] h-screen bg-cover bg-no-repeat px-mainPhone lg:px-mainDesktop flex items-center`}>
+    <div className={`${bgImage} py-[100px] bg-cover bg-no-repeat px-mainPhone md:px-[50px] lg:px-mainDesktop flex flex-col-reverse lg:flex-row items-center`}>
       {children}
     </div>
   );

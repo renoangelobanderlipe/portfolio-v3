16b3faacaa22c0ba0b25e1fbd5f95109a08cdcbb
Reno Angelo <personal.renoangelo@gmail.com>
2023-12-08 10:10:29 +0800

    refs #15 added temporary data while waiting for the API

---
+++ src/services/api/data.js
@@ -0,0 +1,18 @@
+export const heroSection = {
+  'title': "Hi, I'm Reno Angelo",
+  'description': "Lorem Ipsum dolor sit amet",
+  'my_works': {
+    'title': 'Browse my works',
+    'url': '',
+  },
+  'contacts': {
+    'title': 'Reach out to me',
+    'url': ''
+  },
+}
+
+export const aboutSection = {
+  'title': 'A Web Artisan',
+  'description': "Hello, I'm Reno Angelo – a passionate web developer with expertise in both front-end and back-end technologies. Specializing in crafting robust and secure APIs, I bring a commitment to industry standards and a creative touch to every project. Explore my portfolio for a glimpse into seamless, innovative web solutions.",
+  'section': '01. ABOUT ME'
+}

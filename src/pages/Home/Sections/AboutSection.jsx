f4cee98d2d58931bd59f7e865c863da07e86044d
Reno Angelo <personal.renoangelo@gmail.com>
2023-12-08 10:11:46 +0800

    refs #15 added temporary data while waiting for the API

--- src/pages/Home/Sections/AboutSection.jsx
+++ src/pages/Home/Sections/AboutSection.jsx
@@ -1,8 +1,10 @@
 import { Typography } from '@material-tailwind/react';
-import React from 'react';
 import { AppLayout } from '../../../components/ui/AppLayout';
+import { aboutSection } from '../../../services/api/data';
 
 export const AboutSection = () => {
+  const { title, description, section } = aboutSection;
+
   return (
     <AppLayout hasImage={false}>
       <div className='flex lg:flex-row flex-col gap-[84px]'>
@@ -15,18 +17,18 @@ export const AboutSection = () => {
             variant="h5"
             className="flex items-center text-[16px] lg:text-[24px] font-fira text-primary lowercase"
           >
-            01. ABOUT ME
-          </Typography> 
+            {section}
+          </Typography>
           <Typography
             variant="h2"
             className="flex items-center font-main text-text capitalize font-[700] text-[32px] lg:text-[48px]">
-            A Web Artisan
+            {title}
           </Typography>
           <div>
             <Typography
               variant="h1"
               className="flex items-center font-main text-text capitalize font-[400] text-[18px] lg:text-[20px]">
-              Hello, I'm Reno Angelo – a passionate web developer with expertise in both front-end and back-end technologies. Specializing in crafting robust and secure APIs, I bring a commitment to industry standards and a creative touch to every project. Explore my portfolio for a glimpse into seamless, innovative web solutions.
+              {description}
             </Typography>
           </div>
         </div>
--- src/pages/Home/Sections/HeroSection.jsx
+++ src/pages/Home/Sections/HeroSection.jsx
@@ -3,8 +3,12 @@ import { EnvelopeIcon, RightArrowIcon } from '../../../utils/icons';
 import Lottie from 'lottie-react';
 import HeroSectionLottie from '../../../assets/json/hero-section.json'
 import { AppLayout } from '../../../components/ui/AppLayout';
+import { heroSection } from '../../../services/api/data';
+import { Link } from 'react-router-dom';
 
 export const HeroSection = () => {
+  const { title, description, my_works, contacts } = heroSection;
+
   return (
     <AppLayout>
       <div className="flex flex-col gap-[48px] w-full">
@@ -13,28 +17,32 @@ export const HeroSection = () => {
             variant="h5"
             className="flex items-center font-fira font-[16px] lg:font-[24px] text-primary capitalize"
           >
-            Hi, I'm Reno Angelo
+            {title}
           </Typography>
           <Typography
             variant="h1"
             className=" text-[48px] lg:text-[64px] font-[700] max-w-[535px] flex items-center font-main text-text capitalize">
-            Lorem Ipsum dolor sit amet
+            {description}
           </Typography>
         </div>
 
         <div className='flex flex-col-reverse lg:flex-row gap-[24px]'>
-          <Button variant="text" className="flex gap-2 justify-center lg:justify-start rounded-full shadow-none px-0 ">
-            <RightArrowIcon />
-            <Typography className="flex items-center font-fira text-[18px] text-primary capitalize">
-              Browse My Works
-            </Typography>
-          </Button>
-          <Button variant="filled" className="flex gap-2 rounded-full justify-center lg:justify-start bg-primary shadow-none">
-            <EnvelopeIcon />
-            <Typography className="flex items-center font-fira text-[18px] text-icon capitalize">
-              Reach out to me
-            </Typography>
-          </Button>
+          <Link to={my_works.url}>
+            <Button variant="text" className="flex gap-2 justify-center lg:justify-start rounded-full shadow-none px-0 ">
+              <RightArrowIcon />
+              <Typography className="flex items-center font-fira text-[18px] text-primary capitalize">
+                {my_works.title}
+              </Typography>
+            </Button>
+          </Link>
+          <Link to={contacts.url}>
+            <Button variant="filled" className="flex gap-2 rounded-full justify-center lg:justify-start bg-primary shadow-none">
+              <EnvelopeIcon />
+              <Typography className="flex items-center font-fira text-[18px] text-icon capitalize">
+                {contacts.title}
+              </Typography>
+            </Button>
+          </Link>
 
         </div>
       </div>
